a299994432b0fb5c278b8b3473b0a360555f3c27
igor <20102236@mail.wit.ie>
2024-12-03 19:09:14 +0000

    added auth middleware

--- movies-api/index.js
+++ movies-api/index.js
@@ -5,6 +5,8 @@ import usersRouter from './api/users/index.js';
 import './db/index.js';
 import defaultErrHandler from './errHandler/index.js'
 import moviesRouter from './api/movies/index.js';   
+import authenticate from './authenticate/index.js';
+
 
 dotenv.config();
 
@@ -14,7 +16,7 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use('/api/users', usersRouter);
-app.use('/api/movies', moviesRouter); 
+app.use('/api/movies', authenticate, moviesRouter);
 app.use(defaultErrHandler);
 
 app.listen(port, () => {
