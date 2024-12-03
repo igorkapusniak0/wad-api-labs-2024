cc4e3ba7154c8122e0df6a2b2352f81fec585500
igor <20102236@mail.wit.ie>
2024-12-03 18:48:06 +0000

    salt and hash

--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
-
+import bcrypt from 'bcrypt';
 const Schema = mongoose.Schema;
 
 const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+
 
 const UserSchema = new Schema({
   username: { type: String, unique: true, required: true },
@@ -17,6 +19,31 @@ const UserSchema = new Schema({
         'Password must be at least 8 characters long and contain at least one letter, one digit, and one special character.',
     },
   },
+});
+
+UserSchema.methods.comparePassword = async function (passw) { 
+  return await bcrypt.compare(passw, this.password); 
+}
+
+UserSchema.statics.findByUserName = function (username) {
+  return this.findOne({ username: username });
+};
+
+UserSchema.pre('save', async function(next) {
+  const saltRounds = 10; // You can adjust the number of salt rounds
+  //const user = this;
+  if (this.isModified('password') || this.isNew) {
+    try {
+      const hash = await bcrypt.hash(this.password, saltRounds);
+      this.password = hash;
+      next();
+  } catch (error) {
+     next(error);
+  }
+
+  } else {
+      next();
+  }
 });
 
 export default mongoose.model('User', UserSchema);
